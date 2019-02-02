faf5290d77f9af941954d4051d55cc55adb63428
Lucas Youngers <lucasyoungers@Adams-MacBook-Pro.local>
2019-02-01 18:40:28 -0800

    added randomName.js

---
+++ randomName.js
@@ -0,0 +1,58 @@
+// start by constructing a variable that is an array of objects, this is our data.
+let people = [{
+    "id": 1,
+    "firstName": "Frank",
+    "lastName": "Herbert",
+    "job": "Lead Software Engineer",
+    "Skills": ["JavaScript", "C#", "SQL", "HTML", "CSS", "SCRUM Master"],
+    "Salary": 120196
+},
+{
+    "id": 2,
+    "firstName": "Joan",
+    "lastName": "Armorett",
+    "job": "Jr Software Developer",
+    "Skills": ["JavaScript", "HTML", "CSS"],
+    "Salary": 70000
+},
+{
+    "id": 3,
+    "firstName": "John",
+    "lastName": "Mosses",
+    "job": "Software Engineer",
+    "Skills": ["JavaScript", "C#", "SQL", "HTML", "CSS"],
+    "Salary": 101234
+},
+{
+    "id": 4,
+    "firstName": "Janet",
+    "lastName": "Lee",
+    "job": "Software Engineer",
+    "Skills": ["JavaScript", "C#", "HTML", "CSS", "Technical Writer"],
+    "Salary": 86000
+},
+{
+    "id": 5,
+    "firstName": "Garry",
+    "lastName": "Funny",
+    "job": "Quality Assurance Engineer",
+    "Skills": ["JavaScript", "C#", "QA", "C#"],
+    "Salary": 89100
+},
+{
+    "id": 6,
+    "firstName": "Karen",
+    "lastName": "Enoch",
+    "job": "Jr Software Engineer",
+    "Skills": ["JavaScript", "C#", "HTML", "CSS"],
+    "Salary": 70000
+},
+{
+    "id": 7,
+    "firstName": "Jason",
+    "lastName": "Keel",
+    "job": "Quality Assurance Engineer",
+    "Skills": ["JavaScript", "C#", "SQL"],
+    "Salary": 98000
+}
+];
